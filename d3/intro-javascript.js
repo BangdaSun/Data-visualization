4a9849af3e3ec1c4ca79a94481d6bc96cf3c5296
Bangda Sun <bangdasun94@gmail.com>
2017-06-11 18:38:40 -0400

    Create intro-javascript.js

---
+++ d3/intro-javascript.js
@@ -0,0 +1,13 @@
+
+// Hello Javascript
+console.log('hello javascript');
+
+/*
+  variables: use 'var' keyword 
+    character / string
+    numbers
+    boolean
+*/
+var myString = 'bangda';
+var myNumber = 23;
+var myBool   = true;
