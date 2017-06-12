68398df3efed270f6db27af5900e4c46b498f223
Bangda Sun <bangdasun94@gmail.com>
2017-06-11 22:26:14 -0400

    Create intro-javascript.js

--- d3/intro-javascript.js
+++ d3/intro-javascript.js
@@ -1,3 +1,6 @@
+/*
+  Javascript tutorial from codecademy
+*/
 
 // Hello Javascript
 console.log('hello javascript');
@@ -11,3 +14,60 @@ console.log('hello javascript');
 var myString = 'bangda';
 var myNumber = 23;
 var myBool   = true;
+
+console.log('My name is: ' + myString + ', I am ' + myNumber + ' year\'s old now.');
+console.log(myBool);
+
+// Control flow
+if (myNumber >= 18) {
+  console.log(myName + ' is an adult now.');
+}
+else {
+  console.log(myName + ' is not an adult now.')
+}
+
+if (myString === 'bangda') { // use === here to denote equal of strings
+  console.log('This is bangda.');
+}
+else if (myString === 'jiahui') {
+  console.log('This is jiahui');
+}
+else {
+  console.log('who are you?');
+}
+
+switch (myString) {
+  case 'bangda':
+    console.log('This is bangda.');
+  case 'jiahui':
+    console.log('This is jiahui.');
+  default:
+    console.log('Who are you?')
+}
+
+// Loop
+var sum = 0;
+for (var i = 1; i <= 100; i++){
+  sum = sum + i;
+}
+
+while (i <= 100) {
+  sum = sum + i;
+  i ++;
+}
+
+// Function
+function printName(name) {
+  console.log(name);
+}
+
+function squareofX(x) {
+  return x * x;
+}
+
+// Array
+var arr = ['one', 'two', 'three'];
+console.log('first element of arr: ' + arr[0]);
+console.log('last element of arr: ' + arr[2]);
+console.log('length of arr (array): ' + arr.length);
+console.log('length of arr[0] (string): ' + arr[1].length);
