493adf25f38c9cb896769b2937b16c88d549fa2d
Bangda Sun <bangdasun94@gmail.com>
2017-06-19 00:01:39 -0400

    Update oop.js

--- d3/oop.js
+++ d3/oop.js
@@ -39,4 +39,54 @@ var myObject = {
   male: true,
   type: 'Most excellent',
   interests: ['Programming', 'Basketball', 'Cooking']
-};
+}; // keep in mind use colon : rather than assignement sign =
+
+// Anothe example
+var friends = new Object();
+friends.bangda = {}; // we can add properties into bangda, equivalent to friends.bangda = new Object()
+friends[jiahui] = {}; // we can add properties into jiahui
+
+// New loop for object
+/*
+  for (var key in object) {
+    ...
+  }
+  
+  one example:
+*/
+var friends = new Object();
+friends.bill = {};
+friends.steve = {};
+
+friends.bill = {
+    firstName : 'Bill',
+    lastName : 'James',
+    number : '23',
+    address : ['169 Manhattan Ave', '3A']
+    }
+    
+friends.steve = {
+    firstName : 'Steve',
+    lastName : 'Harden',
+    number : '13',
+    address : ['169 Manhattan Ave', '4A']
+    }
+
+// list(): list info of object
+var list = function(object) {
+    for (var key in object) {
+        console.log(key);
+    }
+}
+
+list(friends.bill)
+
+// search() function: take a name, find the mathced first name in friends
+var search = function(name) {
+    for (var key in friends) {
+        if (friends[key].firstName === name) {
+            console.log(friends[key]);
+            return friends[key];
+        }
+    }
+}
