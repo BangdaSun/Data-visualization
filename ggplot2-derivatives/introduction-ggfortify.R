735886dbdf2b9b335f2a03aeb9031e527f5271ac
Bangda Sun <bangdasun94@gmail.com>
2017-07-29 07:27:36 -0400

    Update introduction-ggfortify.R

--- ggplot2-derivatives/introduction-ggfortify.R
+++ ggplot2-derivatives/introduction-ggfortify.R
@@ -33,3 +33,35 @@ ggplot2::autoplot(density(rnorm(1:100)), fill = 'green')
 str(eurodist)
 ggplot2::autoplot(eurodist) + 
   theme(axis.text.x = element_text(angle = 60, hjust = 1))
+
+
+### 4. Time series
+#   autoplot() can handle various time-series objects include zoo, xts, ts
+#   Single series
+class(AirPassengers)
+head(AirPassengers)
+ggplot2::autoplot(AirPassengers)
+ggplot2::autoplot(AirPassengers, ts.colour = 'red', ts.linetype = 'dashed')  # color and colour is different here
+ggplot2::autoplot(AirPassengers, ts.geom = 'bar')
+
+#   Multivariate series
+library(vars)
+data(Canada)
+class(Canada)
+Canada[1:10, 4]  # matrix like object
+
+ggplot2::autoplot(Canada)
+ggplot2::autoplot(Canada, facets = FALSE)
+
+#   work with forecast package
+library(forecast)
+ts_model1 <- auto.arima(AirPassengers)
+ts_model1_pred <- forecast(ts_model1, level = c(95), h = 25)
+ggplot2::autoplot(ts_model1)
+ggplot2::autoplot(ts_model1_pred, predict.colour = 'red', predict.linetype = 'dashed')
+
+#   work with changepoint package
+library(changepoint)
+
+
+#   work with Time series statistics
