e1fc60fe774dde0fef7d070b9e6db5f2a13a2875
Bangda Sun <bangdasun94@gmail.com>
2017-08-16 19:18:47 -0400

    Update base-graphics.R

--- base-graphics.R
+++ base-graphics.R
@@ -14,14 +14,14 @@ abline(lm(mpg ~ wt))
 detach(mtcars)
 dev.off()
 
-#   symbol and line: specify with number or name
+# symbol and line: specify with number or name
 # * type 
 # * pch
 # * cex
 # * lty
 # * lwd
 
-#   color: we can specify it with number, color name, hexdecimal, RGB, HSV
+# color: we can specify it with number, color name, hexdecimal, RGB, HSV
 # * col
 # * col.axis
 # * col.lab
@@ -34,7 +34,7 @@ dev.off()
 # create continuous color vector: rainbow(), heat.colors(), terrain.colors(), topo.colors()
 
 
-#   text: font size, font, font style (bold...)
+# text: font size, font, font style (bold...)
 # * cex
 # * cex.axis
 # * cex.lab
@@ -46,12 +46,12 @@ dev.off()
 # * ps (size)
 # * family: serif, sans, mono
 
-#   figure position and size
+# figure position and size
 # * pin: width, height
 # * mai: (down, left, up, right)
 # * mar: 
 
-#   axis: specify axis style with axis()
+# axis: specify axis style with axis()
 # * side: position to put axis
 # * at: position of ticks
 # * labels: ticks label
@@ -64,34 +64,34 @@ plot(x, y, type = 'p')
 axis(side = 1, tck = 1)
 axis(side = 2, tck = 1)
 
-#   reference line: abline()
+# reference line: abline()
 
-#   legend:
+# legend:
 # * location
 # * title
 # * legend
 
-#   text note: text()
+# text note: text()
 # * location
 # * pos (1, 2, 3, 4)
 # * side
 
 ?plotmath
 
-#   also we can include these args into par() 
+# also we can include these args into par() 
 
-#   multiple-plots
-#   use par() and layout()
+# multiple-plots
+# use par() and layout()
 
-#   par(mfrow = c(nrows, ncols)), mfrow can be nfcol = ..
+# par(mfrow = c(nrows, ncols)), mfrow can be nfcol = ..
 
-#   layout(mat), mat is used to specify location, e.g.: (fig 1 at row 1, fig 2 and 3 at row 2, 2x2 layout)
+# layout(mat), mat is used to specify location, e.g.: (fig 1 at row 1, fig 2 and 3 at row 2, 2x2 layout)
 layout(matrix(c(1, 1, 2, 3), 2, 2, byrow = TRUE), widths = c(3, 1), heights = c(1, 2))
 
 
 ### 1. Bar plot
 
-#   Simpliest case
+# Simpliest case
 library(vcd)
 head(Arthritis)
 counts <- with(Arthritis, table(Improved))
@@ -106,7 +106,7 @@ barplot(counts, main = 'horizontal barplot',
 # or we can just use plot() if the data is factor
 with(Arthritis, plot(Improved))
 
-#   Multiple groups
+# Multiple groups
 counts <- with(Arthritis, table(Improved, Treatment))
 counts  # matrix like object
 
@@ -120,7 +120,7 @@ barplot(counts, main = 'Stacked barplot',
   col = c('red', 'yellow', 'green'),
   legend = rownames(counts), beside = TRUE)
 
-#   Combined with aggregation data
+# Combined with aggregation data
 states <- data.frame(state.region, state.x77)
 head(states)
 aggr_mean <- aggregate(states$Illiteracy, by = list(state.region), mean)
@@ -128,7 +128,7 @@ aggr_mean
 aggr_mean_ordered <- aggr_mean[order(aggr_mean$x), ]
 aggr_mean_ordered
 
-#   Customize barplot
+# Customize barplot
 # * cex.names --> font size
 # * names.arg --> labels
 # * las = ..  --> rotate labels
