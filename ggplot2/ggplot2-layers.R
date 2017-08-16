d82102716f85e3c98d1f01f2a64985e477d77964
Bangda Sun <bangdasun94@gmail.com>
2017-08-16 19:31:19 -0400

    Update ggplot2-layers.R

--- ggplot2/ggplot2-layers.R
+++ ggplot2/ggplot2-layers.R
@@ -3,18 +3,18 @@
 ###############################################
 
 ### 4.1 Intro
-#   Five elements of layers of plot
+# Five elements of layers of plot
 #   (1) data
 #   (2) mapping - aesthetic
 #   (3) geom object
 #   (4) statistical transformation
 
 ### 4.2 Create plot object
-#   start using ggplot()!!
+# start using ggplot()!!
 
 ### 4.3 Layers
-#   using + to add layers:
-#   layers(geom, geom_param, stat, stat_param, data, mapping, position)
+# using + to add layers:
+# layers(geom, geom_param, stat, stat_param, data, mapping, position)
 p = ggplot(diamonds, aes(x = carat))
 p = p + layer(
   geom = "bar",
@@ -24,7 +24,7 @@ p = p + layer(
 )
 p
 
-#   in short:
+# in short:
 p = ggplot(diamonds, aes(x = carat)) +
   geom_bar(fill = "red") + 
   stat_bin(bins = 50)
@@ -32,13 +32,13 @@ p
 summary(p)
 
 ### 4.4 Data
-#   HAVE TO USE data frame in ggplot
+# HAVE TO USE data frame in ggplot
 
 ### 4.5 Aesthetics
-#   aes()
-#   for example: aes(x = weight, y = height, colour = age)
-#   it will map x coord to weight, y coord to height, colour to age
-#   4.5.1 plot and layer
+# aes()
+# for example: aes(x = weight, y = height, colour = age)
+# it will map x coord to weight, y coord to height, colour to age
+##  4.5.1 plot and layer
 p = ggplot(mtcars)
 summary(p)
 p
@@ -46,31 +46,31 @@ p = p + aes(wt, hp)
 summary(p)
 p
 
-#   use '+' to add layers
+# use '+' to add layers
 p = ggplot(mtcars, aes(x = mpg, y = wt)) +
   geom_point()
 p
-#   revise layers aesthetics
+# revise layers aesthetics
 p1 = p + geom_point(aes(color = factor(cyl)))
 p1
 p2 = p + geom_point(aes(y = disp))
 p2
 
-#   4.5.2 setting and mapping
-#   we can also assgin param separately
-#   two different examples!
+##  4.5.2 setting and mapping
+# we can also assgin param separately
+# two different examples!
 p = ggplot(mtcars, aes(mpg, wt))
 print(p + geom_point(aes(color = "darkblue")))
 print(p + geom_point(color = "darkblue"))
 
-#   4.5.3 group
-#   geom object can be classified as 
-#   (1) individual - point ... one object for one obs
-#   (2) collective - histogram ... one object for multi obs
-#   to control which obs use which geom object, we use group
+##  4.5.3 group
+# geom object can be classified as 
+# (1) individual - point ... one object for one obs
+# (2) collective - histogram ... one object for multi obs
+# to control which obs use which geom object, we use group
 
-#   example 1: multi groups and single aesthetic
-#   Oxboys dataset: record 26 boys' (subject) height and age at 9 occasions
+# example 1: multi groups and single aesthetic
+# Oxboys dataset: record 26 boys' (subject) height and age at 9 occasions
 library(nlme)
 head(Oxboys, 15)
 summary(Oxboys)
@@ -80,11 +80,11 @@ ggplot(Oxboys, aes(age, height, group = Subject)) +
 ggplot(Oxboys, aes(age, height, color = Subject)) +
   geom_line()
 
-#    incorrect example
+# incorrect example
 ggplot(Oxboys, aes(age, height)) + geom_line()
 
-#    example 2: different groups on different layers
-#    sometimes we want summarize different levels of data to plot
+# example 2: different groups on different layers
+# sometimes we want summarize different levels of data to plot
 p = ggplot(Oxboys, aes(age, height, group = Subject)) +
   geom_line()
 p + geom_smooth(aes(group = Subject), method = "lm", se = F)
@@ -94,9 +94,9 @@ p + geom_smooth(aes(group = 1), method = "lm", size = 2, se = F)
 ### 4.6 Geometric object
 
 ### 4.7 Statistical transformation
-#   summary the data, for example: smooth
+# summary the data, for example: smooth
 
-#   use ..var.. to avoid name conflict
+# use ..var.. to avoid name conflict
 ggplot(diamonds, aes(carat)) +
   geom_histogram(aes(y = ..density..), binwidth = .05)
 
@@ -107,11 +107,11 @@ ggplot(diamonds, aes(carat)) +
   geom_histogram(aes(y = ..density..), binwidth = .5)
 
 ### 4.8 Position
-#   dodge: side by side
-#   fill: height set to 1
-#   identity
-#   jitter
-#   stack
+# dodge: side by side
+# fill: height set to 1
+# identity
+# jitter
+# stack
 
 p = ggplot(diamonds, aes(clarity))
 p + geom_bar(aes(fill = cut))  # default to be stack
