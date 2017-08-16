693b97c85bacfbc040eb6d4e1115aba25c199399
Bangda Sun <bangdasun94@gmail.com>
2017-08-16 19:23:52 -0400

    Update introduction-ggfortify.R

--- ggplot2-derivatives/introduction-ggfortify.R
+++ ggplot2-derivatives/introduction-ggfortify.R
@@ -9,12 +9,12 @@
 library(ggfortify)
 
 ### 1. Plotting diagnostics for linear models
-#   ggfortify let ggplot2 know how to interpret lm objects
+# ggfortify let ggplot2 know how to interpret lm objects
 lm_model1 <- lm(Petal.Width ~ Petal.Length, data = iris)
 ggplot2::autoplot(lm_model1)
 ggplot2::autoplot(lm_model1, which = 1:6, ncol = 3)
 
-#   some decorations
+# some decorations
 ggplot2::autoplot(lm_model1, which = 1:6, ncol = 3, 
                   colour = 'dodgerblue3',  # can be treated as aesthetics
                   smooth.color = 'black',
@@ -36,15 +36,15 @@ ggplot2::autoplot(eurodist) +
 
 
 ### 4. Time series
-#   autoplot() can handle various time-series objects include zoo, xts, ts
-#   Single series
+# autoplot() can handle various time-series objects include zoo, xts, ts
+# Single series
 class(AirPassengers)
 head(AirPassengers)
 ggplot2::autoplot(AirPassengers)
 ggplot2::autoplot(AirPassengers, ts.colour = 'red', ts.linetype = 'dashed')  # color and colour is different here
 ggplot2::autoplot(AirPassengers, ts.geom = 'bar')
 
-#   Multivariate series
+# Multivariate series
 library(vars)
 data(Canada)
 class(Canada)
@@ -53,15 +53,15 @@ Canada[1:10, 4]  # matrix like object
 ggplot2::autoplot(Canada)
 ggplot2::autoplot(Canada, facets = FALSE)
 
-#   work with forecast package
+# work with forecast package
 library(forecast)
 ts_model1 <- auto.arima(AirPassengers)
 ts_model1_pred <- forecast(ts_model1, level = c(95), h = 25)
 ggplot2::autoplot(ts_model1)
 ggplot2::autoplot(ts_model1_pred, predict.colour = 'red', predict.linetype = 'dashed')
 
-#   work with changepoint package
+# work with changepoint package
 library(changepoint)
 
 
-#   work with Time series statistics
+# work with Time series statistics
